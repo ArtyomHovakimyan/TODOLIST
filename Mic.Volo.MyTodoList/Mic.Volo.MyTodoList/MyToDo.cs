7b2f2997c253c22da57115ce34b545ba2b747a76
User <artyom.hovakimyan01@gmail.com>
2019-04-09 15:30:23 +0400

    some error

---
+++ Mic.Volo.MyTodoList/Mic.Volo.MyTodoList/MyToDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mic.Volo.MyTodoList
+{
+    class MyToDo : IComparable<MyToDo>
+    {
+        public static int ID { get; set; } = 0;
+        public int id;
+        public DateTime CreateTime { get; set; }
+        public string Content { get; set; }
+        public enum SaveAs { Active, Completed };
+        public SaveAs Save = SaveAs.Active;
+        public bool IsActive => Save == SaveAs.Active;
+        static MyToDo()
+        {
+            ID++;
+        }
+        public MyToDo()
+        {
+            ID = id + 1; 
+            id = ID;
+            CreateTime = DateTime.Now;
+            Content = "";
+        }
+        public MyToDo(string str, DateTime time,SaveAs save = SaveAs.Active)
+        {
+            Content = str;
+            CreateTime = time;
+            Save = save;
+        }
+        public override string ToString()
+        {
+            return "[ID]"+"["+id+"]"+Content + " Created " + CreateTime;
+        }
+        public int CompareTo(MyToDo other)
+        {
+            return MyToDo.ID - other.id;
+        }
+    }
+}
+
--- Mic.Volo.MyTodoList/Mic.Volo.MyTodoList/Program.cs
+++ Mic.Volo.MyTodoList/Mic.Volo.MyTodoList/Program.cs
@@ -10,6 +10,70 @@ namespace Mic.Volo.MyTodoList
     {
         static void Main(string[] args)
         {
+            List<MyToDo> MyToDosTask = new List<MyToDo>();
+            MyLabel:
+            Console.WriteLine("If you want create  new  todo  press 1");
+            Console.WriteLine("If  you has todo press 2 to show  all");
+            Console.WriteLine("If  you has active todo press 3 to show");
+            Console.WriteLine("delete todo  press 4 and  enter Id ");
+            Console.WriteLine("show completed todos");
+            string mutq;
+            int indexID;
+            byte n;
+            n = byte.Parse(Console.ReadLine());
+            switch (n)
+            {
+                case 1: Console.WriteLine("Enter your text:");
+            mutq = Console.ReadLine();
+                    MyToDosTask.Add(new MyToDo(mutq,DateTime.Now));
+                    goto default;
+                   // break;
+                case 2: Show(MyToDosTask);
+                    goto default;
+                    //break;
+
+                case 3: ShowIsActive(MyToDosTask);
+                    goto default;
+                // break;
+                case 4:
+                    Console.WriteLine("enter ID which you want to delete:");
+                    indexID = int.Parse(Console.ReadLine());
+                    MyToDosTask.RemoveAt(indexID);
+                    goto default;
+                    //break;
+                default: goto MyLabel;
+            }
+
+
         }
+        static void ShowIsActive(List<MyToDo> mies)
+        {
+            var f = from i in mies
+                    where i.IsActive
+                    select i;
+            foreach (var item in f)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Show(List<MyToDo> mies)
+        {
+            var f = from i in mies
+                    select i;
+            foreach (var item in f)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Completed(List<MyToDo> mies)
+        {
+            var f = from i in mies
+                    select i;
+            foreach (var item in f)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
     }
 }
